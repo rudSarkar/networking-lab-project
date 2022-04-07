49d160dbf6530585e75fc788c3953beefa2504f1
Rudra Sarkar <rudrasarkar815@gmail.com>
2022-04-07 16:01:17 +0600

    config updated

--- app/config.py
+++ app/config.py
@@ -14,9 +14,8 @@ class DevConfig:
 
 
 class ProdConfig:
-    def __init__(self):
-        self.SECRET_KEY = os.getenv("PROD_SECRET_KEY")
-        self.MYSQL_HOST = os.getenv("PROD_MYSQL_HOST")
-        self.MYSQL_USER = os.getenv("PROD_MYSQL_USER")
-        self.MYSQL_PASSWORD = os.getenv("PROD_MYSQL_PASSWORD")
-        self.MYSQL_DB = os.getenv("PROD_MYSQL_DB")
+    SECRET_KEY = os.getenv("PROD_SECRET_KEY")
+    MYSQL_HOST = os.getenv("PROD_MYSQL_HOST")
+    MYSQL_USER = os.getenv("PROD_MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("PROD_MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("PROD_MYSQL_DB")
