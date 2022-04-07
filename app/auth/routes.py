829586d3860a045dd398daf2f1448808017c1511
Rudra Sarkar <rudrasarkar815@gmail.com>
2022-04-07 15:43:08 +0600

    html templated added and configuration changed

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,38 @@
-from flask import render_template
+import re
 from . import auth
+from ..models.users import Users
+from flask import render_template, redirect, url_for, request, flash
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html'), 200
+    elif request.method == "POST":
+        test = "nano"
+        return 'POST Login', 200
+    else:
+        return 'Something went wrong or this method is\'t allowed', 405
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-	return render_template('signup.html')
+    if request.method == 'POST':
+        full_name = request.form['fullname']
+        user_name = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        user = Users.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email address already exists')
+            return redirect(url_for('auth.signup'))
+
+        create_user = Users(
+            fullname=full_name,
+            username=user_name,
+            email=email,
+            password=password
+        )
+    return render_template('signup.html'), 200
--- app/config.py
+++ app/config.py
@@ -1,18 +1,22 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class DevConfig():
+
+class DevConfig:
     SECRET_KEY = os.getenv("DEV_SECRET_KEY")
     MYSQL_HOST = os.getenv("DEV_MYSQL_HOST")
     MYSQL_USER = os.getenv("DEV_MYSQL_USER")
     MYSQL_PASSWORD = os.getenv("DEV_MYSQL_PASSWORD")
     MYSQL_DB = os.getenv("DEV_MYSQL_DB")
 
-class ProdConfig():
-    SECRET_KEY = os.getenv("PROD_SECRET_KEY")
-    MYSQL_HOST = os.getenv("PROD_MYSQL_HOST")
-    MYSQL_USER = os.getenv("PROD_MYSQL_USER")
-    MYSQL_PASSWORD = os.getenv("PROD_MYSQL_PASSWORD")
-    MYSQL_DB = os.getenv("PROD_MYSQL_DB")
+
+class ProdConfig:
+    def __init__(self):
+        self.SECRET_KEY = os.getenv("PROD_SECRET_KEY")
+        self.MYSQL_HOST = os.getenv("PROD_MYSQL_HOST")
+        self.MYSQL_USER = os.getenv("PROD_MYSQL_USER")
+        self.MYSQL_PASSWORD = os.getenv("PROD_MYSQL_PASSWORD")
+        self.MYSQL_DB = os.getenv("PROD_MYSQL_DB")
