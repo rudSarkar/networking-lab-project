5e33be826978ff2dce55792b651e808d56dde42a
Rudra Sarkar <rudrasarkar815@gmail.com>
2022-04-09 14:45:43 +0600

    removed flask_login module from project

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,5 @@
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
-login_manager = LoginManager()
